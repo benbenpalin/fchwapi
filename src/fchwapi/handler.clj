8898129007c088d1040a4721d35f78ff34b0ec52
Ben Palin <benpalin1@gmail.com>
2021-08-09 00:08:22 -0600

    return only fields necessary to make the basic profile

--- src/fchwapi/handler.clj
+++ src/fchwapi/handler.clj
@@ -7,19 +7,27 @@
     [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
     [fchwapi.config :refer [api-key]]))
 
+(defn pull-basic-profile [profile]
+  (let [profile-map (json/read-str profile)]
+    (merge
+      (select-keys profile-map ["fullName" "avatar" "title" "organization"])
+      (select-keys (get profile-map "details") ["phones" "emails" "profiles"]))))
+
 (defn call-enrich [customer-info-type customer-info]
-  (:body (client/post "https://api.fullcontact.com/v3/person.enrich"
-                      {:headers      {:Authorization api-key}
-                       :content-type :json
-                       :form-params  {customer-info-type customer-info}})))
+  (pull-basic-profile
+    (:body
+      (client/post "https://api.fullcontact.com/v3/person.enrich"
+        {:headers      {:Authorization api-key}
+         :content-type :json
+         :form-params  {customer-info-type customer-info}}))))
 
 (defroutes app-routes
   (POST "/enrich" req
     (let [type (get-in req [:params :customerInfoType])
           info (get-in req [:params :customerInfo])]
-      {:status  200
+      {:status   200
        :headers {"Content-Type" "application/json"}
-       :body (call-enrich type info)}))
+       :body    (json/write-str (call-enrich type info))}))
   (route/not-found "Not Found"))
 
 (def app
