fcf609e3d32c81858ed8c5703c520f31cc28b3c4
Ben Palin <benpalin1@gmail.com>
2021-08-08 23:36:31 -0600

    get customer information from front end and send it to enrich api

--- src/fchwapi/handler.clj
+++ src/fchwapi/handler.clj
@@ -7,20 +7,20 @@
     [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
     [fchwapi.config :refer [api-key]]))
 
-
-(defn call-enrich []
+(defn call-enrich [customer-info-type customer-info]
   (:body (client/post "https://api.fullcontact.com/v3/person.enrich"
-                    {:headers {:Authorization api-key}
-                     :content-type :json
-                     :form-params  {:phone "+13035551234"}})))
+                      {:headers      {:Authorization api-key}
+                       :content-type :json
+                       :form-params  {customer-info-type customer-info}})))
 
 (defroutes app-routes
-  (POST "/enrich" []
-    {:status  200
-     :headers {"Content-Type" "application/json"}
-     :body (call-enrich)})
+  (POST "/enrich" req
+    (let [type (get-in req [:params :customerInfoType])
+          info (get-in req [:params :customerInfo])]
+      {:status  200
+       :headers {"Content-Type" "application/json"}
+       :body (call-enrich type info)}))
   (route/not-found "Not Found"))
-
 
 (def app
   (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
